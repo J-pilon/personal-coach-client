012e1014a87214ea74356fce9530cea99381283a
Josiah <josiah.pilon@gmail.com>
2025-07-11 09:09:34 -0700

    chore: setup for testing

---
+++ jest.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  preset: 'jest-expo',
+  setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
+  transformIgnorePatterns: [
+    'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)'
+  ],
+  testPathIgnorePatterns: ['/node_modules/', '/android/', '/ios/'],
+  testEnvironment: 'node',
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/$1'
+  },
+  collectCoverageFrom: [
+    '**/*.{ts,tsx}',
+    '!**/node_modules/**',
+    '!**/coverage/**',
+    '!**/*.config.js',
+    '!**/index.ts',
+    '!**/App.tsx'
+  ],
+  setupFiles: ['<rootDir>/jest.setup.js']
+}; 
