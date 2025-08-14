00dcbd21013a52a00321a0a3cc0586887e3086b7
Josiah <josiah.pilon@gmail.com>
2025-08-13 18:33:18 -0700

    s: dynamically set API_BASE_URL based on environment

--- constants/config.ts
+++ constants/config.ts
@@ -2,16 +2,21 @@ import Constants from 'expo-constants';
 
 const getApiBaseUrl = () => {
   // Get from Expo config
-  const expoApiUrl = Constants.expoConfig?.extra?.apiBaseUrl;
+  const expoConfig = Constants.expoConfig?.extra?.apiBaseUrl;
   
-  if (expoApiUrl) {
-    return expoApiUrl;
+  if (expoConfig) {
+    // Use __DEV__ to determine environment
+    const environment = __DEV__ ? 'development' : 'production';
+    const url = expoConfig[environment];
+    
+    if (url) {
+      return url;
+    }
   }
   
   // Fallback
-  if (__DEV__) {
-    return 'http://localhost:3000/api/v1';
-  }
+  if (__DEV__) return 'http://localhost:3000/api/v1'
+  
   return 'https://personal-coach-server-c405d49500bf.herokuapp.com/api/v1';
 };
 
